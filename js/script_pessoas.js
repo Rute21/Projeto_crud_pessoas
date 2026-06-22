cc5976fb2d99809fce9dd94458044c1c69be2c76
Ana Rute Da Silva Santos <ana.silvasantos@aluno.senai.br>
2026-06-22 16:36:56 -0300

    atualizações no css/style_pessoas.css,  index.html e no  js/script_pessoas.js

--- js/script_pessoas.js
+++ js/script_pessoas.js
@@ -1,3 +1,6 @@
+//IMPORTANDO ARQUIVOS
+import {calcDesconto} from "./script_calculos.js"
+
 //PEGANDO ELEMENTOS DO DOM
 const formPessoa = document.querySelector('#form-pessoa')
 const divLista = document.querySelector('#div-lista-pessoas')
@@ -48,7 +51,7 @@ divLista.innerHTML = ''
 //PERCORRER O ARRAY pessoas COM O foreach
 pessoas.forEach((elem, i) => {
     divLista.innerHTML += `${i + 1} - ${elem.nome} - ${elem.idade} idade, ${parseFloat(elem.renda)
-    .toFixed(2).replace('.',',')} <br>`
+    .toFixed(2).replace('.',',')} ${calcDesconto(elem)} <br>`
 })
 
 }
